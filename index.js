0673fa67c18adc7d198891ec1e9d3ccecbbc9886
Moisés Macías Díaz <moises@mardesin.com>
2025-06-03 20:28:46 +0200

    MySQL, rutas y variables de entorno

--- index.js
+++ index.js
@@ -1,46 +1,90 @@
 let express = require("express"); // Importamos express
 let app = express(); // Creamos una instancia de express
+let mysql = require("mysql2");
+let path = require("path");
+require("dotenv").config({ path: "./.env" });
 
-// console.log("Primer mensaje");
+app.use(express.static(path.join(__dirname, "public"))); // Configuramos la carpeta public como carpeta estatica
 
-// let nombre = "Moi";
-// let apellidos = "Macías Díaz";
-
-// console.log("Mi nombre es " + nombre + " " + apellidos);
-
-// let dias = [
-//     "Lunes",
-//     "Martes",
-//     "Miercoles",
-//     "Jueves",
-//     "Viernes",
-//     "Sabado",
-//     "Domingo",
-// ];
-
-// let colors = require("colors");
-
-// console.log("Hoy es ".red + dias[2].green);
-
-// let calculos = require("./funciones.js");
-
-// calculos.sumar(1, 2);
-// calculos.restar(1, 2);
-// calculos.multiplicar(1, 2);
-// calculos.dividir(1, 2);
-
-// let http = require("http");
-
-// http.createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.write("<h1>Hola Mundo</h1>");
-//     console.log("petición recibida");
-//     res.end();
-// }).listen(4000);
-
-app.get("/", function (req, res) {
-    res.send("Hola Mundo");
+let conexion = mysql.createConnection({
+    //Creamos la configuración de la base de datos
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
-app.listen(4000);
-console.log("Servidor escuchando en el puerto 4000");
+conexion.connect(function (error) {
+    //Conectamos con la base de datos
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Conectado a la base de datos");
+    }
+});
+
+// visualizamos los profesores
+conexion.query(
+    "SELECT nombre, apellidos FROM profesores",
+    function (error, resultados) {
+        if (error) {
+            console.log(error);
+        } else {
+            // console.log(resultados);
+            resultados.forEach((profesor) => {
+                console.log(profesor.nombre + " " + profesor.apellidos);
+            });
+        }
+    }
+);
+
+// //insertar
+// conexion.query(
+//     "INSERT INTO profesores (nombre, apellidos, codigo_curso) VALUES ('Pepe', 'Perez', 2)",
+//     function (error, resultados) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log("Registro insertado ", resultados);
+//         }
+//     }
+// );
+
+// // actualizar
+// conexion.query(
+//     "UPDATE profesores SET apellidos = 'Garcia' WHERE id = 12",
+//     function (error, resultados) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log("Registro actualizado ", resultados);
+//         }
+//     }
+// );
+
+// //borrar
+// conexion.query(
+//     "DELETE FROM profesores WHERE id = 13",
+//     function (error, resultados) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log("Registro borrado ", resultados);
+//         }
+//     }
+// );
+
+conexion.end();
+
+app.get("/", function (req, res) {
+    // res.send("Hola Mundo");
+    res.sendFile(path.join(__dirname, "public", "pages/index.html"));
+});
+
+app.get("/contacto", function (req, res) {
+    // res.send("Página de contacto");
+    res.sendFile(path.join(__dirname, "public", "pages/contacto.html"));
+});
+
+app.listen(process.env.PORT);
+console.log("Servidor escuchando en el puerto 4000 http://localhost:4000");
